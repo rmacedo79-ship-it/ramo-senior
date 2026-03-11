31c10f961531c23ce531cfdce795d3eb51353a03
rmacedo79-ship-it <rmacedo79@gmail.com>
2026-03-11 16:45:17 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'senior-v1';
+const assets = [
+  './programa_senior_escoteiros.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
